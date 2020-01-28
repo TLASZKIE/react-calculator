dba0632bd89c4319bd34b9fb5cb072bce43d1b16
Tomek Łaszkiewicz <tomasz.laszkiewicz@avanade.com>
2020-01-28 12:16:19 +0100

    add watch and real test

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -1,10 +1,11 @@
-test("should 2+2 be 4", () => {
-  const result = 2 + 2;
-  expect(result).toBe(4);
+import { getFormattedValue } from "../utils";
+
+test("should format value correctly", () => {
+  expect(getFormattedValue("1234.0")).toBe("1,234.0");
 });
 
-describe("scope", () => {
-  test('should "a" be true', () => {
-    expect("a").toBeTruthy();
-  });
+test("should 2 + 2 be 4", () => {
+  expect(2 + 2).toBe(4);
+  let someObj = { a: 10, b: 20 };
+  expect(someObj).toEqual({ a: 10, b: 20 });
 });
