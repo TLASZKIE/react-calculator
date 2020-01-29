9619b4b7412e6034223dc64b0cd4e39ffa82b02c
Tomek Łaszkiewicz <tomasz.laszkiewicz@avanade.com>
2020-01-29 09:35:25 +0100

    Remove yarn.lock and messy staff

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -8,14 +8,4 @@ test("should 2 + 2 be 4", () => {
   expect(2 + 2).toBe(4);
   let someObj = { a: 10, b: 20 };
   expect(someObj).toEqual({ a: 10, b: 20 });
-
-  const mockFn = jest
-    .fn()
-    .mockReturnValue("default")
-    .mockReturnValueOnce("1")
-    .mockReturnValueOnce("2");
-
-  console.log(mockFn("yo"), mockFn("lo"), mockFn());
-  console.log(mockFn.mock.calls);
-  console.log(mockFn.mock.results);
 });
